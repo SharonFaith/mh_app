53963ad11e0e1aff07d1a0a9ffdfa2679cd30e06
SharonFaith <sharonfaith15@gmail.com>
2020-12-09 18:45:50 +0300

    add user model

--- mh_app/models.py
+++ mh_app/models.py
@@ -1,1 +1,69 @@
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.hashers import make_password
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, first_name, last_name, password, **other_fields):
+        if not email:
+            raise ValueError('You must provide an email address ')
+        if not username:
+            raise ValueError('Username is required')
+        if not first_name:
+            raise ValueError('You must provide your first_name')
+        if not last_name:
+            raise ValueError('You must provide your last_name')
+        if not password:
+            raise ValueError('Password is required')
+        
+        email = self.normalize_email(email)
+        user = self.model(email = email, username = username, first_name=first_name, last_name=last_name,
+                         password=password, **other_fields)
+        #user.setpassword(password)
+        user.set_password(password)
+        user.save()
+        return user
+    def create_superuser(self, email, username, first_name, last_name, password, **other_fields):
+
+        if not username:
+            raise ValueError('Username is required')
+        if not first_name:
+            raise ValueError('You must provide your first_name')
+        if not last_name:
+            raise ValueError('You must provide your last_name')
+        
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must be assigned to is_superuser=True.')        
+
+        return self.create_user(email, username, first_name, last_name, password, **other_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    date_registered = models.DateTimeField(auto_now = True)
+
+    # For the system admin
+    is_superuser = models.BooleanField(default=False)
+
+    # For the mh professional
+    is_staff = models.BooleanField(default=False)
+    #for all users
+    is_active = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
 
